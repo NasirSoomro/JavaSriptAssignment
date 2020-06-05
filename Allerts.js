c9949d601efd2a257ce47b13661690a8d5e901c4
unknown <engrnasirs18@gmail.com>
2020-06-05 09:04:21 +0500

    Now ready to commit

---
+++ Allerts.js
@@ -0,0 +1,18 @@
+alert("Error!please Enter a Valid Password.");
+if("Error!please Enter a Valid Password."){
+    alert("Welcome to JS land... \n Happy Coding!");
+}
+if("Welcome to JS land... \n Happy Coding!"){
+    alert("Welcome to JS land...  ");
+}
+if("Welcome to JS land... "){
+    alert(" Happy Coding! \n Prevent this page from creating additional dialogs");
+}
+ 
+
+
+  var x = "Hello...I can run JS through my web browser's cosole";
+ 
+
+   console.log (x);
+
---
+++ chapter2.js
@@ -0,0 +1,42 @@
+var myName = ( "Nasir Hidayat Soomro");
+alert(myName);
+
+var message = "Hello World";
+if("Nasir Hidayat Soomro"){
+    alert(message)
+}
+var name = "Jhone Doe";
+if("Hello World"){
+    alert(name)
+}
+var  age = "15 years old";
+if("Jhone Doe"){
+    alert(age)
+}
+ 
+  var education = "Certified Mobile Application Developer";
+ if("15 years old"){
+  alert(education)
+ }
+
+
+var pzz = ("PIZZA \n PIZZ \n PIZ \n PI \n P")
+  if("Certified Mobile Application Developer"){
+      alert(pzz)
+  }
+  var adress= ("My email adress is engrnasirs18@gmail.com" );
+ if("PIZZA \n PIZZ \n PIZ \n PI \n P")
+  alert(adress);
+
+
+  var book = ("I am trying to learn from the Book A smarter way to learn JavaScript");
+
+
+  if("My email adress is engrnasirs18@gmail.com" )
+  alert(book)
+  var disply = ("Write a script to display this in browser through JS");
+  document.write(disply);
+var line = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+if("Write a script to display this in browser through JS"){
+    alert(line)
+}
