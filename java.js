2f14c8b04af5e5c6401c53f93881d3989649e323
unknown <engrnasirs18@gmail.com>
2020-06-14 17:42:53 +0500

    Now ready to commit

--- java.js
+++ java.js
@@ -719,11 +719,11 @@
 
 // Task #  06 : b
 
-var counting = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-for(i=1; i<=15; i++){
-  document.write(counting.sort((a,b)=>b-a))
-  break;1
-}
+// var counting = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+// for(i=1; i<=15; i++){
+//   document.write(counting.sort((a,b)=>b-a))
+//   break;1
+// }
 
 
 
